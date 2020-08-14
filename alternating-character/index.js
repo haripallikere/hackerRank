ceab1b7084bb32139bb72c6122880b5b945cfe89
haripallikere <haripallikere93@gmail.com>
2020-08-14 13:48:47 +0530

    pr changes

--- alternating-character/index.js
+++ alternating-character/index.js
@@ -1,10 +1,25 @@
-function alternatingCharacters(s) {
-    let a  = s.split('');
-    let count = 0;
+
+// Count of the same adjacent characters in a string 
+  // Parameters - valid string, inputString(str)
+  // Return - count of same adjacent character, adjacentCount(int)
+
+//test cases
+//   print(alternatingCharacters('abcd') === 0);
+//   print(alternatingCharacters('aabbcc') === 3);
+//   print(alternatingCharacters('adefccbbaabb') === 4);
+ 
+
+function alternatingCharacters(inputString) {
+    let a  = inputString.split('');
+    let adjacentCount = 0;
     for (let i = 0; i< a.length; i++) {
-        if (a[i] === a[i+1]) {
-            count += 1
+    if (a[i] === a[i+1]) {
+            adjacentCount += 1
         } 
     }
-    return count;
+   return adjacentCount;
 }
+
+// learning
+    // string split method
+    // for-loop and index
